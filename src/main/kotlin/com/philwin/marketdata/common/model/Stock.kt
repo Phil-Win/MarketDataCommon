62dff874e6835004774c8cb39611eaf278c9a21f
phil <nguye208@gmail.com>
2020-01-13 17:50:33 -0500

    Updating the Stock Repository

--- src/main/kotlin/com/philwin/marketdata/common/model/Stock.kt
+++ src/main/kotlin/com/philwin/marketdata/common/model/Stock.kt
@@ -14,39 +14,9 @@ data class Stock (
     var close: BigDecimal? = null,
     var high: BigDecimal? = null,
     var low: BigDecimal? = null,
-    var bid: BigDecimal? = null,
-    var ask: BigDecimal? = null,
-    var percent_change: BigDecimal? = null,
-    var volume: Long? = null,
-    var high_52_week: BigDecimal? = null,
-    var low_52_week: BigDecimal? = null
+    var volume: Long? = null
 ){
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
 }
-
-
-
-//@Entity
-//@Table(name = "stock")
-//class Stock {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    var id: Long? = null
-//    var symbol: String? = null
-//    @Temporal(TemporalType.DATE)
-//    var date: Date? = null
-//    var open: BigDecimal? = null
-//    var close: BigDecimal? = null
-//    var high: BigDecimal? = null
-//    var low: BigDecimal? = null
-//    var bid: BigDecimal? = null
-//    var ask: BigDecimal? = null
-//    var percent_change: BigDecimal? = null
-//    var volume: Long? = null
-//    var high_52_week: BigDecimal? = null
-//    var low_52_week: BigDecimal? = null
-//
-//    constructor(){}
-//}
--- src/main/kotlin/com/philwin/marketdata/common/repository/StockRepository.kt
+++ src/main/kotlin/com/philwin/marketdata/common/repository/StockRepository.kt
@@ -4,11 +4,14 @@ import com.philwin.marketdata.common.model.Stock
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-
+import java.util.Date
 @Repository
-interface StockRespository : CrudRepository<Stock, Long> {
+interface StockRepository : CrudRepository<Stock, Long> {
     fun findBySymbol(symbol: String): List<Stock>
 
     @Query("SELECT DISTINCT s.symbol FROM Stock s ")
     fun findUniqueSymbols() : List<String>
+
+    @Query("SELECT MAX(s.date) FROM Stock s where s.symbol = ?1")
+    fun findLatestDate(symbol: String) : Date?
 }
